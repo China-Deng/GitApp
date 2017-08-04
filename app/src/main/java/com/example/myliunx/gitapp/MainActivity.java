7350357112381ae4a9c76387d615f98f0eba7e19
伤城幻化 <987415780@qq.com>
2017-08-04 13:57:27 +0800

    1.one

--- app/src/main/java/com/example/myliunx/gitapp/MainActivity.java
+++ app/src/main/java/com/example/myliunx/gitapp/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.myliunx.gitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
+/**
+ * zhushi
+ */
 public class MainActivity extends AppCompatActivity {
-
+    Button btu ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
