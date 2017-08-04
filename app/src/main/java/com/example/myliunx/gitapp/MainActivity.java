d93f9b5f408788b0137d91b76f26be4c4c0b0779
伤城幻化 <987415780@qq.com>
2017-08-04 14:04:32 +0800

    1.one

--- app/src/main/java/com/example/myliunx/gitapp/MainActivity.java
+++ app/src/main/java/com/example/myliunx/gitapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
  * zhushi
  */
 public class MainActivity extends AppCompatActivity {
-    Button btu ,btu1,btu3;
+    Button btu ,btu1,btu3,btu4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
